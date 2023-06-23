f7115b6a5a482eaab4973569aef9b1f4c9bd85ee
Harsh Parmar <harshparmar4760@gmail.com>
2023-06-23 22:16:01 +0530

    save

---
+++ string.java
@@ -0,0 +1,25 @@
+public class string {
+        public static void main(String[] args) {
+    String name1 = new String("Aman");
+    String description = new String("is a good boy.");
+
+    String sentence = name1 + description;
+    System.out.println(sentence);
+// CharAt
+// String name = new String("Aman");
+// System.out.println(name.charAt(0));
+
+// Length
+// String name = new String("Aman");
+// System.out.println(name.length());
+
+// Replace
+// String name = new String("Aman");
+// System.out.println(name.replace('a', 'b'));
+
+// Substring
+// String name = new String("AmanAndAkku");
+// System.out.println(name.substring(0, 4));
+}
+
+}
