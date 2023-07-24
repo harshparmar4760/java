905604f189526d966eef120c415c6154392e480b
Harsh Parmar <harshparmar4760@gmail.com>
2023-07-25 01:01:55 +0530

    a

--- methods/string.java
+++ methods/string.java
@@ -1,12 +1,22 @@
 package methods;
 
+import java.util.Scanner;
+
 public class string {
     public static void main(String[] args) {
         // String message = greet();
         // System.out.println(message);
 
-        String personalised = myGreet("harsh parmar");
+        // String personalised = myGreet("harsh parmar");
+        // System.out.println(personalised);
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.next();
+        String personalised = myGreet(name);
         System.out.println(personalised);
+
+        
     }
     static String myGreet(String name){
         String message = "hello " + name;
@@ -18,3 +28,4 @@ public class string {
         return greeting;
     }
 }
+ 
